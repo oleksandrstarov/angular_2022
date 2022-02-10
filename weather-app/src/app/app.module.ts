65eb8572cc4ea83cbdab3e6d14b20c00892959c1
Irina Litvinova <irina.litvinova@sigma.software>
2022-02-10 17:01:43 +0200

    update console log message

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -17,6 +17,6 @@ import { AppComponent } from './app.component';
 })
 export class AppModule implements OnInit {
   ngOnInit(): void {
-    console.log('The app is initialized')
+    console.log('Console log from develop branch')
   }
  }
