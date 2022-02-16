a69b3e537f39410be74e38a0d14589d42457f184
Vira Velmozhna <viravelmozhna@gmail.com>
2022-02-16 11:52:29 +0200

    loader component created

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TestComponent } from './test/test.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TestComponent,
-    FooterComponent
+    FooterComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
